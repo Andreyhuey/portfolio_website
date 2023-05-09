a4307ff2129a8c31b0675c625b0c5dac3c08483b
Andreyhuey <andreyhuey777@gmail.com>
2023-05-09 22:39:53 +0100

    Hero section modified

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        I am a Front End Web Developer with a passion for building aesthetically
+        beautiful & functional web applications.
+      </SectionText>
+      <Button
+        onClick={() => (window.location = "mailto:andreyhuey777@gmail.com")}
+      >
+        Contact Me
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
